e3922f97c13ecd57de9d112c29e6249032b9617b
Damien Aicheh <damienaicheh@microsoft.com>
2024-07-28 16:33:54 +0200

    new image

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "azurerm_resource_group" "this" {
-  name     = format("rg-%s", "crazy")
-  location = "francecentral"
-  tags     = {
-    "tag1": "value1"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "st123rfgdzad345"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
   }
 }
 
